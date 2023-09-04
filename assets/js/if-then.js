946deeaadb9180566df960eafa2167a01e048cb0
dkanders12 <dkanders12@gmail.com>
2023-09-04 12:42:05 +0200

    t

--- assets/js/if-then.js
+++ assets/js/if-then.js
@@ -1,40 +1,45 @@
-
 // variabler du skal bruge i din kode...
-const A = 10;
-const B = 30;
-const C = '30';
-const D = 10;
-const E = 40;
+const varA = 10;
+const varB = 30;
+const varC = 30;
+const varD = 10;
+const varE = 40;
 
 // Opgave 1: skriv et expression der viser true når A er lig med B.
 
-if ( /*..dit expression Her*/ ) {
-    console.log('statement true!');
+if (varA + varB) {
+  console.log("statement true! A=B");
 }
 
-
 //opgave 2: skrive en If then statement der vurderer om A er større end B.
-
-
-
+if (varA > varB) {
+  console.log("satemanet true! A>B");
+} else {
+  console.log("stement false!");
+}
 
 //opgave 3: Skriv en If then statement der vurderer om A er mindre end eller lig med B
-
-
-
-
-
+if (varA < varB) {
+  console.log("satemanet true! A<B");
+}
 //opgave 4: Skriv en If then statement der bliver FALSE når du sammenligner B og C.
-
-
-
-
-
+if (varA == varB) {
+  console.log("statement true! A=B");
+} else {
+  console.log("stement false!");
+}
 //opgave 5: Skriv en If then statement der bliver TRUE når du sammenligner B og C.
-
-
-
-
-//opgave 6: Skriv en If then statement der sammenligner B = C og E = A hvis B=C skal den blive TRUE. 
-
-
+if (varA == varD) {
+  console.log("statement true! A=D");
+}
+//opgave 6: Skriv en If then statement der sammenligner B = C og E = A hvis B=C skal den blive TRUE.
+if (varA == varE) {
+  console.log("statement true! A=E");
+} else {
+  console.log("stement false!");
+}
+if (varB == varC) {
+  console.log("statement true! B=C");
+} else {
+  console.log("stement false!");
+}
--- assets/js/operators.js
+++ assets/js/operators.js
@@ -1,27 +1,41 @@
-
 // variabler du skal bruge i din kode...
 const A = 10;
 const B = 30;
-const C = '30';
+const C = 30;
 const D = 10;
 const E = 40;
 
 // Opgave 1: skriv et expression der viser true når A er lig med B eller A er lig med D.
 
-if ( /*..dit expression Her*/ ) {
-    console.log('statement true!');
+if (A == B) {
+  console.log("statement true!");
+} else {
+  console.log("statement False");
 }
 
-
 //opgave 2: skrive en If then statement der bliver TRUE når  A er større end B og A er mindre end E.
-
-
-
-
+if (A > B) {
+  console.log("statement true!");
+} else {
+  console.log("statement False");
+}
+if (A < E) {
+  console.log("statement true!");
+} else {
+  console.log("statement False");
+}
 //opgave 3: Skriv en If then statement med  && og  || der kun er sand når alle expressions er sande.
-
-
-
-
+if ((A && B) || E) {
+  console.log("statement true!");
+} else {
+  console.log("statement False");
+}
 
 //opgave 4: Skriv en If then statement der kan finde ud af om F er "undefined" eller har en værdi, ved at bruge !.
+var F;
+
+if (!F) {
+  console.log("F er undefined eller har ingen værdi.");
+} else {
+  console.log("F har en værdi.");
+}
